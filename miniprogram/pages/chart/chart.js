6fa40d25e24c7c75122e9986fc5b9f9967034abb
K-xw <361577873@qq.com>
2019-04-19 00:47:26 +0800

    fourth

--- miniprogram/pages/chart/chart.js
+++ miniprogram/pages/chart/chart.js
@@ -1,4 +1,6 @@
 // pages/chart/chart.js
+
+const util = require('../../utils/utils.js')
 Page({
 
     /**
@@ -6,13 +8,18 @@ Page({
      */
     data: {
         isActive: true, //true 是支出  false 是收入
+        date: '本月',
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
+        let nowDate = util.formatDateMonth(new Date());
 
+        this.setData({
+            date: nowDate
+        })
     },
 
     /**
@@ -22,26 +29,7 @@ Page({
 
     },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
+   
 
     /**
      * 页面相关事件处理函数--监听用户下拉动作
@@ -56,11 +44,34 @@ Page({
     onReachBottom: function () {
 
     },
+    //日期选择器函数
+    bindDateChange(e) {
+        let seleDate = e.detail.value;
+        let nowDate = util.formatDateMonth(new Date());
+        console.log('picker发送选择改变，携带值为', seleDate);
+        console.log(nowDate)
 
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
+        /** 为iphone6 时间选择器 做兼容处理 */
+        var year = seleDate.split('-')[0]
+        var mon = seleDate.split('-')[1]
+        if (year.length == 2) {
+            seleDate = 2000 + parseInt(year, 10)
+            seleDate = seleDate + '-' + mon;
+        }
+        console.log("兼容之后的：", seleDate)
 
+        if (seleDate != nowDate) {
+            var seleM = seleDate.split('-')[1];
+            this.setData({
+                date: seleM + "月",
+            })
+        } else {
+            var seleM = seleDate.split('-')[1];
+            this.setData({
+                date: '本月',
+            })
+        }
+        
     }
+   
 })
